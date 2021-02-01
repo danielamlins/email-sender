0afaa63189f68a67e9f8fa16b249651fe89e5278
Daniela Lins <danielamlins@gmail.com>
2021-02-01 21:03:42 +0100

    fixed address enabling CORS

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "portifolio.danielalins.com");
+  res.header("Access-Control-Allow-Origin", "https://portifolio.danielalins.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
