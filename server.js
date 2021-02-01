1fd292bb88f85d4da5b44b49add6c624e4fe383b
Daniela Lins <danielamlins@gmail.com>
2021-02-01 20:58:02 +0100

    enable CORS

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ app.use(bodyParser.urlencoded());
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "portifolio.danielalins.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post("/email", function (req, res) {
   body = req.body;
   email = body.email;
