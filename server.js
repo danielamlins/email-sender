ceb6139c1ed64ff8cc1796724c87450a4d2d445a
Daniela Lins <danielamlins@gmail.com>
2021-02-02 15:05:56 +0100

    changed app.use

--- server.js
+++ server.js
@@ -6,15 +6,19 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "OPTIONS,POST");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, access-control-allow-origin");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requeted-With, Content-Type, Accept, Authorization, RBR");
+  if (req.headers.origin) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+  }
+  if (req.method === 'OPTIONS') {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    return res.status(200).json({});
+  }
   next();
-});
+ });  
 
 // let corsOptionsDelegate = function (req, callback) {
 //   let corsOptions;
